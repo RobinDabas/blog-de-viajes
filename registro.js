ee4f1ae47d93c93c61132108c84519217afb5189
RobinDabas <vossosuncapo@gmail.com>
2025-03-23 23:05:33 -0400

    add form and create .js file

---
+++ registro.js
@@ -0,0 +1,17 @@
+
+    const popupRegistro = document.getElementById("popupRegistro");
+    const abrirPopup = document.getElementById("abrirPopup");
+    const cerrarPopup = document.getElementById("cerrarPopup");
+
+    abrirPopup.addEventListener("click", () => {
+        popupRegistro.style.display = "block";
+    });
+
+    cerrarPopup.addEventListener("click", () => {
+        popupRegistro.style.display = "none";
+    });
+
+ 
+    
+
+
